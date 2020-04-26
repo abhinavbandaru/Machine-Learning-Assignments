1e8d733487877b0f88ed098b55f47afa62610cf1
Vatsal Gupta <50845148+G-Vatsal@users.noreply.github.com>
2020-04-26 18:12:20 +0530

    Update Naive_bayes.py

--- Naive_bayes.py
+++ Naive_bayes.py
@@ -1,26 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 15:54:20 2020
+
+@author: LENOVO
+"""
+
 import numpy as np
 import pandas as pd
-import re
-import nltk.tokenize
-import math
-from nltk.stem import PorterStemmer
-porter=PorterStemmer()
 
 dataset=pd.read_csv('a1_d3.txt',sep="\n",header=None,delimiter='\t')
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~+-1234567890'''
 for i in range(len(dataset)):
     X=dataset.iloc[i,0]
+    for var in X.lower():
+        if var in punctuations:
+            X=X.replace(var,'')
     dataset.iloc[i,0] =X.lower()
-    dataset.iloc[i,0] = re.sub(r'\W'," ", dataset.iloc[i,0]) 
-    dataset.iloc[i,0] = re.sub(r'\s+'," ", dataset.iloc[i,0])
-    stem_sentence=[]
-    words=nltk.word_tokenize(dataset.iloc[i,0])
-    for var in words:
-        stem_sentence.append(porter.stem(var))
-        stem_sentence.append(" ")
-    stem_sentence="".join(stem_sentence)
-    dataset.iloc[i,0]=stem_sentence
 
-import operator
 av_accuracy=0
 min_accuracy=1
 max_accuracy=0
@@ -30,13 +26,16 @@ max_fscore=0
 for t in range(1,6):
     split_data=int(t*len(dataset)/5)
     split_data_begin=int((t-1)*len(dataset)/5)
-    X_test=dataset.iloc[split_data_begin:split_data,:]
+    X_test=dataset.iloc[split_data_begin:split_data,:-1]
+    Y_test=dataset.iloc[split_data_begin:split_data,-1].values
     X_train=dataset.iloc[:split_data_begin,:]
-    X_train=pd.concat([X_train,dataset.iloc[split_data:,:]])   
+    X_train=pd.concat([X_train,dataset.iloc[split_data:,:]])
+    Y_train=X_train.iloc[:,-1].values
+    X_train=X_train.iloc[:,:-1]
     
     uniqueword={}
     for sent in X_train.iloc[:,0]:
-        word=nltk.word_tokenize(sent)
+        word=sent.split()
         for var in word:
             if var not in uniqueword:
                 uniqueword[var]=1
@@ -45,44 +44,42 @@ for t in range(1,6):
 
     accuracy_max=0
     best_size=50
-    listofdict=sorted(uniqueword.items(),key=operator.itemgetter(1),reverse=True)
-    listofdict_var=listofdict[:750]
+    listofdict_var=list(uniqueword.items())
     listofdicti=[i[0] for i in listofdict_var]
     X_t=[]
     for sent in X_train.iloc[:,0]:
-        word=nltk.word_tokenize(sent)
+        word=sent.split()
         vector=np.zeros(len(listofdicti))
         for var in word:
             if var in listofdicti:
                 vector[listofdicti.index(var)]=1
         X_t.append(vector)
     X_t=np.asarray(X_t)
-    tot_yes=X_train[1].sum(axis=0)
-    tot_no=len(X_train)-tot_yes
+    tot_yes=Y_train.sum()
+    tot_no=len(Y_train)-tot_yes
     prob_arr_yes=np.zeros((tot_yes,len(listofdicti)))
     prob_arr_no=np.zeros((tot_no,len(listofdicti)))
     count_yes=0
     count_no=0
     for i in range(len(X_train)):
-        if X_train.iloc[i,1]==1:
+        if Y_train[i]==1:
             prob_arr_yes[count_yes]=X_t[i,:]
             count_yes+=1
         else:
             prob_arr_no[count_no]=X_t[i,:]
             count_no+=1
     
-    prob_vec_yes=((prob_arr_yes.sum(axis=0)+1)/len(prob_arr_yes))
-    prob_vec_no=((prob_arr_no.sum(axis=0)+1)/len(prob_arr_no))
+    prob_vec_yes=((prob_arr_yes.sum(axis=0)+1)/(len(prob_arr_yes)))
+    prob_vec_no=((prob_arr_no.sum(axis=0)+1)/(len(prob_arr_no)))
     
     prob_yes=(tot_yes/len(X_train))
     prob_no=(tot_no/len(X_train))
     
     Y_pred=np.zeros(len(X_test))
-    Y_test=np.asarray(X_test.iloc[:,1])
     
     i=0
     for sent in X_test.iloc[:,0]:
-        word=nltk.word_tokenize(sent)
+        word=sent.split()
         prob=1
         for var in word:
             if var in listofdicti:
@@ -136,5 +133,4 @@ diff=max(abs(max_accuracy-av_accuracy),abs(min_accuracy-av_accuracy))
 difffscore=max(abs(max_fscore-av_fscore),abs(min_fscore-av_fscore))
 print("Average Testing Accuracy:",av_accuracy,"+-",round(diff,2))
 print("Average F-Score:",round(fscore,3),"+-",round(difffscore,2))    
-    
                         
